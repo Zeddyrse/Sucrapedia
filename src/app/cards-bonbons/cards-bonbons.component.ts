259408336ed2499490b588feb9f2fcc929fc3d58
Irifus <aymeric.post@gmail.com>
2018-10-30 21:23:19 +0100

    Colonnes de cartes bougées de app.component.html au composant cards-bonbons

--- src/app/cards-bonbons/cards-bonbons.component.ts
+++ src/app/cards-bonbons/cards-bonbons.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
+import { BonbonsService } from '../bonbons.service';
 
 @Component({
   selector: 'app-cards-bonbons',
   templateUrl: './cards-bonbons.component.html',
   styleUrls: ['./cards-bonbons.component.css']
 })
-export class CardsBonbonsComponent implements OnInit {
+export class CardsBonbonsComponent {
 
-  constructor() { }
+   public nom_de_bonbon:any[];
 
-  ngOnInit() {
-  }
+   constructor(param_service:BonbonsService){
+     this.nom_de_bonbon = null;
 
-}
+     param_service.getAvoirBonbon().subscribe(
+       (param_nom_de_bonbon:any[]) => {
+         console.log("coucou",param_nom_de_bonbon);
+         this.nom_de_bonbon = param_nom_de_bonbon;
+       }
+     )
+   }
+ }
