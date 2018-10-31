ebe807d82772f392f747b5c2cf979da81f100733
plumcake <plumcakecoder@gmail.com>
2018-10-31 12:00:01 +0100

    maj search bar

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
 
-  constructor() { }
+  public aImg: string[] = ['Searchs_000.jpg','Searchs_001.jpg','Searchs_003.jpg','Searchs_004.jpg','Searchs_005.jpg'];
+  public tTimer:number ;
 
-  ngOnInit() {
-  }
+ public changeMoiCeFond():any
+{
+   let nSize = this.aImg.length;
+   let nIndex = Math.floor(Math.random()*(nSize+1));
+
+   //Ici faudrait vérifier que le fond qu'on tire aléatoirement n'est pas le meme que le courant ! si c'est le même faudrait faire une récursion ou rajouter enlever 1...
+   document.body.style.background =  "url("+this.aImg[nIndex]+")";
+
+   //changera le fond ttes les 14 secondes
+   this.tTimer = setTimeout("changeMoiCeFond()", 10000);
+}
+
+ 
 
 }
